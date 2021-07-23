835c1e369fb85c9bff5bc94a6b26b6ad1345d3d0
DonNavinS <seneviratnanavin@gmail.com>
2021-07-23 09:45:36 -0400

    Setup first express route to test

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log("now listening"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "working" });
+});
